e0d436ae23a09d8008cf081bb5a72b931ba8d563
Michael <polli104@mail.chapman.edu>
2020-05-19 22:40:06 -0700

    initial project setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace parkitect_workshop_upload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
